4aca7c0a66c4c158138c9a5048055155c02dfad3
boss <343474093@qq.com>
2019-03-30 17:22:28 +0800

    add notes

--- baidu_spider_wendang.py
+++ baidu_spider_wendang.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from selenium import webdriver
+from selenium import webdriver#驱动服务器
 from selenium.webdriver.common.keys import Keys
 import time
-from lxml import etree
-import re
+from lxml import etree#解析文本
+import re#正则
 
 def main():
-    browser = webdriver.Firefox()
+    browser = webdriver.Firefox()#选择驱动的浏览器
     browser.get('https://wenku.baidu.com/view/fbdc423a3968011ca30091f0.html')
-    browser.set_window_size(1024, 6000)
+    browser.set_window_size(1024, 6000)#浏览器窗口大小
     #继续阅读，继续加载
     elem = browser.find_element_by_xpath("/html/body/div[6]/div[2]/div/div/div[2]/div[1]/div/div[2]/div[2]/div[1]/div[6]/div[2]/div[1]/span")
-    elem.click()
-    f = open("pid_data.txt", "w", encoding="utf-8")
+    elem.click()#点击继续加载按钮
+    f = open("pid_data.txt", "w", encoding="utf-8")#如果用with  效果可能更好
+    #with  open  打开文件  省去了我们关闭文件的操作
     for i in range(2, 83):
             print(i)
-            elem = browser.find_elements_by_css_selector("input.page-input")
-            elem[0].clear()
-            elem[0].send_keys(str(i), Keys.RETURN)
-            time.sleep(10)
+            elem = browser.find_elements_by_css_selector("input.page-input")#页码输入框
+            elem[0].clear()#清零
+            elem[0].send_keys(str(i), Keys.RETURN)#输入当前页
+            time.sleep(10)#等待10  模拟人为操作
             page = str(browser.page_source)
-            root = etree.HTML(page)
+            root = etree.HTML(page)#规范H5元素
             p_list = root.xpath("//div[@id='pageNo-%d']//p" % i)
             cnt = 1
 
@@ -41,13 +42,13 @@ def main():
 
                 print(p.text)
 
-                if cnt % 2 != 0:
+                if cnt % 2 != 0:#前面面写的是区域编码，后面写的是地区名字
                     f.write(p.text)
                 else:
                     f.write(" " + p.text + "\n")
                 cnt += 1
-                f.flush()
-    f.close()
+                f.flush()#刷新当前网页
+    f.close()#关闭文件
     # with open("test.html", "w", encoding="utf-8") as f:
     #     f.write(page)
     # page = browser.find_elements_by_css_selector("#pageNo-2")
